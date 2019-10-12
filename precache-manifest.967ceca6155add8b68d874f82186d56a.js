96c15f84161337db5ba2406a2d9b5355a87ca7bf
AtilaDev <atiladevelop@gmail.com>
2019-10-11 23:10:45 -0300

    Updates

---
+++ precache-manifest.967ceca6155add8b68d874f82186d56a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa0532a4d438789819eefc4da9b5d29",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "6028befe87da04d27e56",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0d22b7ffd02f1a6a5c90",
+    "url": "/robofriends/static/css/main.b9099619.chunk.css"
+  },
+  {
+    "revision": "6028befe87da04d27e56",
+    "url": "/robofriends/static/js/2.2d2bf93a.chunk.js"
+  },
+  {
+    "revision": "0d22b7ffd02f1a6a5c90",
+    "url": "/robofriends/static/js/main.241d8cb8.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
